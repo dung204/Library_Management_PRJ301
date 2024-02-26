e0b38a0c485fa3d247b22a79dae2ae40a7a1c92a
dung204 <acezombiev4@gmail.com>
2024-02-26 14:44:21 +0700

    initial commit

---
+++ src/main/java/com/mantrilogix/Prj301Assignment01Application.java
@@ -0,0 +1,13 @@
+package com.mantrilogix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj301Assignment01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prj301Assignment01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mantrilogix/Prj301Assignment01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mantrilogix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj301Assignment01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
