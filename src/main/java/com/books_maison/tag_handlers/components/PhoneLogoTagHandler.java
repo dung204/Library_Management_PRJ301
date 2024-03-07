d3f63bbb65551db5211e25b4b33078bc8d0140ea
dung204 <acezombiev4@gmail.com>
2024-03-08 02:33:58 +0700

    feat: add footer

---
+++ src/main/java/com/books_maison/tag_handlers/components/PhoneLogoTagHandler.java
@@ -0,0 +1,24 @@
+package com.books_maison.tag_handlers.components;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import jakarta.servlet.jsp.JspException;
+import jakarta.servlet.jsp.tagext.TagSupport;
+
+public class PhoneLogoTagHandler extends TagSupport {
+  private Logger logger = Logger.getLogger(PhoneLogoTagHandler.class.getName());
+
+  @Override
+  public int doStartTag() throws JspException {
+    try {
+      pageContext.getOut().print(
+          """
+              <i class="bi bi-telephone-fill"></i>
+              """);
+    } catch (IOException e) {
+      logger.severe(e.getMessage());
+    }
+    return SKIP_BODY;
+  }
+}
